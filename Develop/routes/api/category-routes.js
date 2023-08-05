2d66328cbbedf559469a625482dd600589220bbc
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-08-04 23:44:07 -0400

    added post, put and delete routes to category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -33,16 +33,52 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const categoryId = req.params.id;
+  const { categoryName } = req.body;
+
+  try {
+    const updatedCategory = await Category.update(
+      { categoryName },
+      {
+        where: { id: categoryId }
+      }
+    );
+    if (updatedCategory[0] === 0) {
+      return res.status(404).json({ err: 'Category not found' });
+    }
+    res.json({ message: 'Category updated succesfully' });
+  }catch (err) {
+    console.error('Error updating category:', error);
+    res.status(500).json({ error: 'Failed to update category' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
